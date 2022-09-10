aaa9fe8f6ce43ed92635581ea707789e10c8805f
mcarlagg17 <carla.glezz@gmail.com>
2022-09-10 10:50:43 +0200

    automatic flask commit

--- app_model.py
+++ app_model.py
@@ -6,11 +6,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error
 import numpy as np
+import pygit as git
 
 os.chdir(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+# Route for the GitHub webhook
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./Flask')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route("/", methods=['GET'])
 def hello():
