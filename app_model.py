df3cce54e6006985d950622548af6d3b55e23e22
Alex Recarey <alex@recarey.org>
2022-09-11 21:25:45 +0200

    Partially working. File upload still wip

--- app_model.py
+++ app_model.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, flash, jsonify, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+
 import os
 import pickle
 import pandas as pd
@@ -13,6 +15,13 @@ os.chdir(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'csv', 'pdf', 'txt'}
+# In a real app, the secret key would be in a separate config file and not visible in the source control
+app.config['SECRET_KEY'] = '\xf7\xc4\xd3:\xdfH\x84h\xcd\xa9\xa5t\xbe\x13\x18L\x0e&\xf8\xc8}\xf9\xe9s'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 # Route for the GitHub webhook
 
 @app.route('/git_update', methods=['POST'])
@@ -26,11 +35,43 @@ def git_update():
 
 @app.route("/", methods=['GET'])
 def hello():
-    return "Bienvenido a mi API del modelo advertising (actualizado)"
+    return render_template('index.html')
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    print("testing")
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return '''parece que funciona'''
+
+#
+# return '''
+#         <!doctype html>
+#         <title>Upload new File</title>
+#         <h1>Upload new File</h1>
+#         <form method=post enctype=multipart/form-data>
+#           <input type=file name=file>
+#           <input type=submit value=Upload>
+#         </form>
+#         '''
 
 @app.route('/api/v1/predict', methods=['GET'])
-def predict():
+def predict_original():
 
     model = pickle.load(open('ad_model.pkl','rb'))
     tv = request.args.get('tv', None)
